73cd9a320ee3d36eeac86bcab0bbe2d59cef85fc
Дмитрий Губанов <dimon89033439971@mail.ru>
2021-10-24 19:16:15 +0300

    add ui file

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from dog_shelter_designer import Ui_MainWindow
 
 
-# Press the green button in the gutter to run the script.
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
 if __name__ == '__main__':
-    print_hi('dog_shelter')
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
