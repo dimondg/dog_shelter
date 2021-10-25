32d297e287e01407dd1f6ddddb4643d2d27be5a0
Дмитрий Губанов <dimon89033439971@mail.ru>
2021-10-25 22:37:34 +0300

    add second form ui

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from dog_shelter_designer import Ui_MainWindow
+from dog_shelter_second_form_designer import Ui_SecondWindow
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
@@ -10,9 +11,16 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
 
+class SecondWidget(QMainWindow, Ui_SecondWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
+    ex2 = SecondWidget()
     ex.show()
+    ex2.show()
     sys.exit(app.exec_())
-
