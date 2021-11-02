ace5df67edba1bfac69fe8cf62b111cf986171b8
Дмитрий Губанов <dimon89033439971@mail.ru>
2021-11-03 02:04:11 +0300

    .

--- choose_window.py
+++ choose_window.py
@@ -1,14 +1,13 @@
-import sqlite3
-
 from PyQt5.QtWidgets import QMainWindow
 from dog_shelter_designer import Ui_MainWindow
-from dog_shelter_second_form_designer import Ui_SecondWindow
+from dog_shelter_choose_form_designer import Ui_SecondWindow
 
 
 class SecondWidget(QMainWindow, Ui_SecondWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle("choose")
         self.btn_back.clicked.connect(self.back)
 
     def back(self):
--- main_window.py
+++ main_window.py
@@ -3,19 +3,21 @@ import sqlite3
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QPixmap
 from dog_shelter_designer import Ui_MainWindow
-from second_window import SecondWidget
+from choose_window import SecondWidget
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle("dog shelter")
         self.btn_search.clicked.connect(self.search)
         self.second_window = SecondWidget()
 
     def search(self):
         self.gender = self.gender_edit.text()
         self.breed = self.breed_edit.text()
+
         con = sqlite3.connect("dog_shelter_db")
         cur = con.cursor()
         file_name = cur.execute("""SELECT
@@ -24,9 +26,9 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             breed == :breed)
         """, {'gender': self.gender, 'breed': self.breed}).fetchall()
         con.close()
+
+        print(file_name)
         self.second_window.pixmap = QPixmap(f"images\{file_name[0][0]}.png")
-        print(file_name[0])
         self.second_window.image_lbl.setPixmap(self.second_window.pixmap)
         self.second_window.show()
-
 
