65ef45abf7caa856e7b2ac2f258e3676458e29cf
dimondg <92933233+dimondg@users.noreply.github.com>
2021-11-08 18:05:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from windows.main_window import MyWidget
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
